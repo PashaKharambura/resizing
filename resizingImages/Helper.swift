b31be4dc4b0d10d43a6e8b5c1706f602cc22d14a
pavlokharambura <pasha.harambura@gmail.com>
2017-09-15 19:19:52 +0300

    firstCommit

---
+++ resizingImages/Helper.swift
@@ -0,0 +1,38 @@
+//
+//  Helper.swift
+//  resizingImages
+//
+//  Created by Pavlo Kharambura on 9/15/17.
+//  Copyright © 2017 Pavlo Kharambura. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIImage {
+    func resizedImage(newSize: CGSize) -> UIImage {
+        // Guard newSize is different
+        guard self.size != newSize else { return self }
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0);
+        self.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+    func resizedImageWithinRect(rectSize: CGSize) -> UIImage {
+        let widthFactor = size.width / rectSize.width
+        let heightFactor = size.height / rectSize.height
+        
+        var resizeFactor = widthFactor
+        if size.height > size.width {
+            resizeFactor = heightFactor
+        }
+        
+        let newSize = CGSize(width: size.width/resizeFactor, height: size.height/resizeFactor)
+        let resized = resizedImage(newSize: newSize)
+        return resized
+    }
+}
--- resizingImages/ViewController.swift
+++ resizingImages/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  resizingImages
@@ -7,19 +8,86 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cropButton: UIButton!
+    
+    var newImages = [UIImage]()
+    var dataImages = [Data]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // MARK: EMAIL
+    
+    func configureMailComtroller() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self
+        
+        mailComposerVC.setToRecipients(["pasha.harambura@gmail.com"])
+        mailComposerVC.setSubject("image")
+        mailComposerVC.setMessageBody("body", isHTML: false)
+        
+        for image in newImages {
+            dataImages.append(UIImagePNGRepresentation(image)!)
+        }
+        
+        for data in dataImages {
+            var counter = 1
+            mailComposerVC.addAttachmentData(data, mimeType: "\(counter)/png", fileName: "\(counter).png")
+            counter += 1
+        }
+        
+        return mailComposerVC
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func sendEmail(_ sender: Any) {
+        let mailComposerViewConroller = configureMailComtroller()
+        
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposerViewConroller, animated: true, completion: nil)
+        } else {
+            print("ERROR")
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Open Pgoto Library
+    
+    @IBAction func openLibrary(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+ 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        self.imageView.image = image
+        dismiss(animated:true, completion: nil)
+    }
+    
+    // MARK: Cropping Images
+    
+    func croppingImages(image: UIImage, width: Double, height: Double) {
+        let newImage = image.resizedImage(newSize: CGSize(width: width, height: height))
+        self.newImages.append(newImage)
     }
 
-
+    @IBAction func cropAction(_ sender: Any) {
+        croppingImages(image: imageView.image!, width: 30, height: 30)
+        croppingImages(image: imageView.image!, width: 50, height: 50)
+        
+    }
+    
 }
-
